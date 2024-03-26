935d0aa099cca64a533345b827b366646840aa41
abounaim.elias@gmail.com <elias.abounaim@etu.u-paris.fr>
2024-03-26 07:38:38 +0100

    is_accepting

--- nfa.py
+++ nfa.py
@@ -18,7 +18,10 @@ class nfa():
 
     def is_accept(self, s, states = None, tab = set()):
         if s == None:
-            return tab
+            for st in states:
+                if st.accepting == True:
+                    return True
+            return False
         if states == None:
             tab = self.initial_state.get_transitions_states(s[0])
         else:
@@ -26,17 +29,19 @@ class nfa():
                 for state in st.get_transitions_states(s[0]):
                     tab.add(state)
         if tab == None:
-            return None
+            return False
         if len(s) == 1:
             return self.is_accept(None, tab)
         return self.is_accept(s[1:], tab)
 
 s1 = state(1, False)
 s2 = state(2, True)
-s3 = state(3, True)
+s3 = state(3, False)
 
 s1.set_transitions({'a':{s2, s3}})
 
 a = nfa(s1)
 
-print(a.is_accept('a'))
+states = a.is_accept('a')
+print(states)
+
