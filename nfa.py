ef47c2913ac56bcbd22d3ec61996c52f877a4471
Amadou Oury Diallo <amadou7oury@gmail.com>
2024-03-26 23:18:28 +0100

    add is_accept

--- nfa.py
+++ nfa.py
@@ -2,47 +2,51 @@ class state:
     def __init__(self, state_id, is_accepting):
         self.state_id = state_id
         self.accepting = is_accepting
+        self.transition = {}
 
     def set_transitions(self, t):
         self.transition = t
 
-    def get_transitions_states(self, t):
+    def get_transitions_states(self, c):
         for a,b in self.transition.items():
-            if a == t :
+            if a == c :
                 return b
         return None
 
 class nfa():
     def __init__(self, initial_state):
         self.initial_state = initial_state
+        
+    def is_accept(self, s):
+        prev = {self.initial_state}
+        states = set()
+        for i in range(len(s)) :
+            for st in prev:
+                if(st.get_transitions_states(s[i]) != None):
+                    for state in st.get_transitions_states(s[i]):
+                        states.add(state)
+            prev = states
+            if i != len(s) - 1 : 
+                states = set()
 
-    def is_accept(self, s, states = None, tab = set()):
-        if s == None:
-            for st in states:
-                if st.accepting == True:
-                    return True
-            return False
-        if states == None:
-            tab = self.initial_state.get_transitions_states(s[0])
-        else:
-            for st in states:
-                for state in st.get_transitions_states(s[0]):
-                    tab.add(state)
-        if tab == None:
-            return False
-        if len(s) == 1:
-            return self.is_accept(None, tab)
-        return self.is_accept(s[1:], tab)
-    
+        for st in states:
+            if st.accepting == True:
+                return True
+        return False
 
 s1 = state(1, False)
-s2 = state(2, True)
+s2 = state(2, False)
 s3 = state(3, False)
+s4 = state(4, False)
+s5 = state(5, True)
 
-s1.set_transitions({'a':{s2, s3}})
-#print(s1.get_transitions_states('a'))
+s1.set_transitions({'a':{s1, s2}, 'b':{s1}})
+s2.set_transitions({'a':{s3}, 'b':{s3}})
+s3.set_transitions({'a':{s4}, 'b':{s4}})
+s4.set_transitions({'a':{s5}, 'b':{s5}})
+
 a = nfa(s1)
 
-states = a.is_accept('a')
+states = a.is_accept('abbbb')
 print(states)
 
