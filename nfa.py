3a1ff60c8a120cc62566f36563acd7a6ace14866
Amadou Oury Diallo <amadou7oury@gmail.com>
2024-03-28 15:58:35 +0100

    add partition

--- nfa.py
+++ nfa.py
@@ -1,3 +1,14 @@
+import random
+from itertools import islice
+
+def batched_docs(iterable, n):
+    # batched('ABCDEFG', 3) --> ABC DEF G
+    if n < 1:
+        raise ValueError('n must be at least one')
+    it = iter(iterable)
+    while batch := tuple(islice(it, n)):
+        yield batch
+
 class state:
     def __init__(self, state_id, is_accepting):
         self.state_id = state_id
@@ -12,10 +23,13 @@ class state:
             if a == c :
                 return b
         return None
+    def __str__(self):
+        return str(self.state_id)
 
 class nfa():
-    def __init__(self, initial_state):
+    def __init__(self, initial_state, list_states):
         self.initial_state = initial_state
+        self.list_states = list_states
  
     def is_accept(self, s):
         prev = {self.initial_state}
@@ -33,20 +47,32 @@ class nfa():
             if st.accepting == True:
                 return True
         return False
+    
+    def partition(self):
+        nb_part = random.randint(2, len(self.list_states))
+        # l = []
+        # for i in range(0, len(self.list_states) // nb_part):
+        #     l.append(self.list_states[i :: nb_part])
+
+        # return l
+        return batched_docs(self.list_states, nb_part)
+
+
 
 s1 = state(1, False)
 s2 = state(2, False)
 s3 = state(3, False)
 s4 = state(4, False)
 s5 = state(5, True)
-
 s1.set_transitions({'a':{s1, s2}, 'b':{s1}})
 s2.set_transitions({'a':{s3}, 'b':{s3}})
 s3.set_transitions({'a':{s4}, 'b':{s4}})
 s4.set_transitions({'a':{s5}, 'b':{s5}})
 
-a = nfa(s1)
 
-states = a.is_accept('abbbb')
-print(states)
+a = nfa(s1, [s1, s2, s3, s4, s5])
+print(list(a.partition()))
+
+# states = a.is_accept('abbbb')
+# print(states)
 
