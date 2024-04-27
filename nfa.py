9353c423051e6fff964a909f45274402d64b4a65
abounaim.elias@gmail.com <elias.abounaim@etu.u-paris.fr>
2024-04-27 22:40:38 +0200

    mod

--- nfa.py
+++ nfa.py
@@ -147,8 +147,8 @@ def crossover(partition_1, partition_2):
     len_1 = len(partition_1)
     len_2 = len(partition_2)
 
-    return (partition_1[:len_1 // 3] + partition_2[len_2 // 3:]
-            , partition_2[:len_2 // 3] + partition_1[len_1 // 3:])
+    return (partition_1[:len_1 // 2] + partition_2[len_2 // 2:]
+            , partition_2[:len_2 // 2] + partition_1[len_1 // 2:])
 
 def partition_from_string(s):
     part = []
@@ -213,6 +213,8 @@ def nfa_from_partition(partition, all_states):
 
 def number_to_states(partitions, all_states):
     l = []
+    print(partitions , "partitions")
+    print(len(all_states), "len all_states")
     for p in partitions:
         m = []
         for s in p:
@@ -231,7 +233,6 @@ def fitness_function(p, all_states, m, plus):
     ps = number_to_states(p, all_states)
     nfa = nfa_from_partition(ps, all_states)
 
-
     err = 0
 
     for s in plus : 
@@ -241,7 +242,7 @@ def fitness_function(p, all_states, m, plus):
     for s in m : 
         if nfa.is_accept(s):
             err += 1
-    return 100*err + len(p)
+    return err + len(all_states)
 
 
 def initial_gen(len_gen, list_states, m, plus):
@@ -281,19 +282,11 @@ def next_gen(prev_gen, list_states, states_minus , states_plus, cent_mut=5, cent
     #crossover 
     for _ in range(len_cross):
 
-        [(c1, f1)] = random.choices(prev_gen, weights=a)
-        [(c2, f2)] = random.choices(prev_gen, weights=a)
-
+        [(c1, _)] = random.choices(prev_gen, weights=a)
+        [(c2, _)] = random.choices(prev_gen, weights=a)
         b,c = crossover(c1, c2)
-
-        fb = fitness_function(b, list_states, states_minus, states_plus)
-        fc = fitness_function(c, list_states, states_minus, states_plus)
-
-        print("f1:", f1, " f2:", f2)
-        print("fb : ", fc, " fc : ", fc)
-
-        l.append((b, fb)) 
-        l.append((c, fc))
+        l.append((b, fitness_function(b, list_states, states_minus, states_plus))) 
+        l.append((c, fitness_function(c, list_states, states_minus, states_plus)))
     return l
 
 def best_avg_fitness(g):
@@ -315,9 +308,8 @@ def algo_genetiq(p, m, taille_gen, nb_gen):
     all = sorted(all, key=lambda x: x[1])
     return all
 
-def bundle(res_algo_genetic, all_states):
-
-    p = partition_from_string(res_algo_genetic[len(res_algo_genetic)-1][0][0][0])
+def bundle(res_algo_genetic):
+    p = partition_from_string(res_algo_genetic[len(a)-1][0][0][0])
 
     ps = number_to_states(p,all_states)
 
@@ -326,52 +318,36 @@ def bundle(res_algo_genetic, all_states):
 
 if __name__ == '__main__':
 
-    a = algo_genetiq(['aa', 'aba', 'bbbb', 'ca', 'cccb'], ['b', 'bba', 'cc', 'a', 'abb', 'c', 'acc', 'bbb', 'b'], 20, 100)
+    a = algo_genetiq(['aa', 'aba', 'bbbb', 'ca', 'cccb'], ['b', 'bba', 'cc', 'a'], 50, 10)
 
-    n, all_states = MCA(['aa', 'aba', 'bbbb', 'ca', 'cccb'])
+    mca,all_states = MCA(['aa', 'aba', 'bbbb', 'ca', 'cccb'])
     # print(mca.is_accept('b'))
     # print(mca.is_accept('aba'))
 
     # print(a[len(a)-1][1])
-
-    # print(n.is_accept('b'))
-    # print(n.is_accept('bba'))
-    # print(n.is_accept('cc'))
-    # print(n.is_accept('a'))
-
-    # print(n.is_accept('aa'))
-    # print(n.is_accept('aba'))
-    # print(n.is_accept('bbbb'))
-    # print(n.is_accept('ca'))
-    # print(n.is_accept('cccb'))
-
-
-    print("-----------------------------------")
-
+    print(a[len(a)-1])
+    print("------------------",a[len(a)-1][0][0][0],"--------------------------")
     p = partition_from_string(a[len(a)-1][0][0][0])
 
     ps = number_to_states(p,all_states)
 
     n = nfa_from_partition(ps,all_states)
- 
-    #print_auto(n)
 
-    for i in range(len(a)):
-        print(a[i][1], "---", a[i][2])
+    print(a[len(a)-1])
 
-    # print()
-    # print_auto(n)
+    print()
+    print_auto(n)
 
-    # print(n.is_accept('b'))
-    # print(n.is_accept('bba'))
-    # print(n.is_accept('cc'))
-    # print(n.is_accept('a'))
+    print(n.is_accept('b'))
+    print(n.is_accept('bba'))
+    print(n.is_accept('cc'))
+    print(n.is_accept('a'))
 
-    # print(n.is_accept('aa'))
-    # print(n.is_accept('aba'))
-    # print(n.is_accept('bbbb'))
-    # print(n.is_accept('ca'))
-    # print(n.is_accept('cccb'))
+    print(n.is_accept('aa'))
+    print(n.is_accept('aba'))
+    print(n.is_accept('bbbb'))
+    print(n.is_accept('ca'))
+    print(n.is_accept('cccb'))
 
 
     # s1 = State(1, False, initial_state=True)
